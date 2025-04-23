6d7ad5c70420efd8c9c7a84ef6334f529bde8324
PolZeck <paul.ledieu@gmail.com>
2025-04-23 12:30:57 +0200

    stop et exit proprement dans exec_builtins

---
+++ dep/minishell/parsing/parsing.d
@@ -0,0 +1,20 @@
+obj/minishell/parsing/parsing.o: srcs/parsing/parsing.c \
+  includes/minishell.h includes/../libft/libft.h includes/signals.h \
+  includes/parsing.h includes/pipex_bonus.h includes/errors.h \
+  includes/exec.h includes/builtins.h
+
+includes/minishell.h:
+
+includes/../libft/libft.h:
+
+includes/signals.h:
+
+includes/parsing.h:
+
+includes/pipex_bonus.h:
+
+includes/errors.h:
+
+includes/exec.h:
+
+includes/builtins.h:
--- srcs/exec/exec_builtins.c
+++ srcs/exec/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: pledieu <pledieu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 12:30:39 by pledieu           #+#    #+#             */
-/*   Updated: 2025/04/21 16:06:49 by pledieu          ###   ########lyon.fr   */
+/*   Updated: 2025/04/23 12:30:13 by pledieu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	execute_builtin(t_cmd *cmd, t_data *data)
 			{
 				perror(redir->file);
 				*get_exit_status() = 1;
-				return ;
+				exit(EXIT_FAILURE);
 			}
 			if (save_stdin != -1)
 				close(save_stdin);
