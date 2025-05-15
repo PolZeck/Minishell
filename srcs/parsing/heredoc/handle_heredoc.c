f3c20fae6b0cf138883ce10a0d1232173567bae0
PolZeck <paul.ledieu@gmail.com>
2025-05-15 16:22:53 +0200

    all good for heredoc expasion or not

--- srcs/parsing/heredoc/handle_heredoc.c
+++ srcs/parsing/heredoc/handle_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: pledieu <pledieu@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 09:00:19 by pledieu           #+#    #+#             */
-/*   Updated: 2025/05/15 12:26:07 by pledieu          ###   ########lyon.fr   */
+/*   Updated: 2025/05/15 16:20:00 by pledieu          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void	handle_heredoc(t_cmd *cmd, t_token **tokens, t_data	*data)
 	}
 	if (!check_heredoc_syntax(cmd, tokens))
 		return ;
-	expand = ((*tokens)->quote_type != SINGLE_QUOTE);
+	expand = ((*tokens)->quote_type == NO_QUOTE);
 	filename = NULL;
 	fd = open_heredoc_tmp(&filename);
 	if (fd < 0)
